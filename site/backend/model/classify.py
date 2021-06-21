fa83d4aea12199caf103f6d958de622612b24ac8
Brandon Mommsen <bmmommsen@alaska.edu>
2021-06-21 13:08:35 -0800

    Added missing files

--- site/backend/model/classify.py
+++ site/backend/model/classify.py
@@ -1,15 +1,17 @@
+'''
+returns the class (0 for non-hazardous road, 1 for hazardous road) for a given image
+'''
+
+
 import os
 import sys
 import tensorflow as tf
-from tensorflow import keras
+#from tensorflow import keras
 import numpy as np
 from shapely.geometry.polygon import Polygon
 from shapely.geometry import Point
-
-
-# need these because polygon vertices are relative to the original images, not the cut rectangles
-ORIG_HEIGHT = 960
-ORIG_WIDTH = 1280
+import pickle
+from PIL import Image
 
 
 # Take a rectangular image and return a list of width X width square sub images
@@ -18,10 +20,10 @@ ORIG_WIDTH = 1280
 def cut_squares(img, poly, width=32, slide=32):
     squares = []
     for y in range(0, img.shape[0] - width, slide):
-      for x in range(0, img.shape[1] - width, slide):
-        if square_in_poly(x, y, width, poly):
-          square = img[y:y+width, x:x+width]
-          squares.append(square)
+        for x in range(0, img.shape[1] - width, slide):
+            if square_in_poly(x, y, width, poly):
+                square = img[y:y+width, x:x+width]
+                squares.append(square)
     return squares
 
 
@@ -31,21 +33,20 @@ def square_in_poly(s_ul_x, s_ul_y, s_width, poly):
     c = 0
     poly = Polygon(poly)
     for y in (s_ul_y, s_ul_y + (s_width // 2), s_ul_y + s_width):
-    for x in (s_ul_x, s_ul_x + (s_width // 2), s_ul_x + s_width):
-      if not poly.contains(Point((x,y))):
-        c += 1
+        for x in (s_ul_x, s_ul_x + (s_width // 2), s_ul_x + s_width):
+            if not poly.contains(Point((x,y))):
+                c += 1
     return c < 3
 
 
-if __name__ == '__main__':
+def classify_hzd(image_path, mask):
 
     # check file path to image is valid
-    image_path = sys.argv[1]
     if not os.path.isfile(image_path):
         sys.exit()
 
     # load model from file
-    model = tf.keras.models.load_model('saved_model')
+    model = tf.keras.models.load_model(model_path)
     model.summary()
 
     # load masks from pickle
@@ -55,12 +56,18 @@ if __name__ == '__main__':
     pkl.close()
 
     # split image into squares
-    cam = sys.argv[2]
-    mask_poly = mask_polygons[cam]
-    squares = cut_squares(data[img]['img_data'], mask_poly)
+    k = image_path.split('/')[-1]
+    mask_poly = mask_polygons[mask]
+    data = np.asarray(Image.open(image_path))
+    squares = cut_squares(data, mask_poly)
+    squares = np.array(squares).astype(np.float32) / 255.0
 
     # classify each square
-    classifications = [model.predict(s) for s in squares]
+    classifications = model.predict(squares)
 
-    # log class (0-1, ratio of most common class to total squares)
-    print(classifications) #TODO change, see comment above
+    # log predicted class (0-1, ratio of most common class to total squares)
+    hzd_ratio = sum([1 if a[1] > a[0] else 0 for a in classifications]) / len(classifications)
+    if hzd_ratio >= 0.5:
+        return '1'
+    else:
+        return '0'
--- site/backend/server.js
+++ site/backend/server.js
@@ -1,27 +1,82 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+const fs = require('fs');
 
 const config = {
     name: 'sample-express-app',
     port: 3000,
-    host: '0.0.0.0',
+    host: '0.0.0.0'
 };
 
 const app = express();
 const logger = log({ console: true, file: false, label: config.name });
 
+// const split_test_init = splitTestInit();
+
+// var main_model_flagged = [];
+// var test_model_flagged = [];
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello butthead');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// app.get('/:ex', (req, res) => {
+//     if (req.params.ex === "1") {
+//         res.sendFile(path.join(__dirname + '/index_e1.html'));
+//     } else if (req.params.ex === "2") {
+//         res.sendFile(path.join(__dirname + '/index_e2.html'));
+//     }
+//
+// });
+
+app.get('/data', (req, res) => {
+    res.sendFile(path.join(__dirname + '/model/data.json'));
+});
+
+//
+// app.get('/data_e1', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/model/data_e1.json'));
+// });
+//
+//
+// app.get('/data_e1', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/model/data_e2.json'));
+// });
+
+
+app.get('/images/:cam', (req, res) => {
+    res.sendFile(path.join(__dirname + `/model/imagestore/official/${req.params.cam}`))
+});
+
+
+app.post('/flag/:cam', (req, res) => {
+  let datastore = JSON.parse(fs.readFileSync(path.join(__dirname + '/model/data.json'), 'utf8'));
+  let curr_image_path = path.join(__dirname + `/model/imagestore/official/${req.params.cam}`);
+  let d = datastore[req.params.cam];
+  let newclass = (d.hazardous === "1") ? "0" : "1";
+  let new_image_path = path.join(__dirname + `/model/imagestore/train/${d.fhash}_${req.params.cam.slice(0,-4)}_${newclass}.jpg`);
+  console.log("image path ", curr_image_path); console.log("copy path ", new_image_path);
+  if (fs.existsSync(new_image_path)) {
+      res.send('image already flagged as incorrect');
+  }
+
+  else {
+      fs.createReadStream(curr_image_path).pipe(fs.createWriteStream(new_image_path));
+      res.send('flagged image as incorrect');
+  }
+});
+
+
 app.listen(config.port, config.host, (e)=> {
-    if(e) {
+    if (e) {
         throw new Error('Internal Server Error');
     }
     logger.info(`${config.name} running on ${config.host}:${config.port}`);
